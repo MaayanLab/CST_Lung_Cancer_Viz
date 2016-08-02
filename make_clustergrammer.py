8deb4b8d69c436fb9869668bc2c8abfab1589c5c
cornhundred <doctorcorn2002@gmail.com>
2016-08-02 13:51:37 -0400

    made quick figures of phos ratios all drug treatment, separated Tyr and ST, increased col label size

---
+++ make_clustergrammer.py
@@ -0,0 +1,29 @@
+import time
+start_time = time.time()
+
+from clustergrammer import Network
+net = Network()
+
+# choose tsv file
+####################
+# net.load_file('txt/phos_ratios_all_treat_no_geld_ST.txt')
+net.load_file('txt/phos_ratios_all_treat_no_geld_Tyrosine.txt')
+
+inst_name = 'Tyrosine'
+
+net.swap_nan_for_zero()
+
+views = ['N_row_sum', 'N_row_var']
+
+net.make_clust(dist_type='cos',views=views , dendro=True,
+               sim_mat=True, filter_sim=0.1, calc_cat_pval=False)
+               # run_enrichr=['KEA_2015'])
+               # run_enrichr=['ENCODE_TF_ChIP-seq_2014'])
+               # run_enrichr=['GO_Biological_Process_2015'])
+
+net.write_json_to_file('viz', 'json/'+inst_name+'.json', 'no-indent')
+net.write_json_to_file('sim_row', 'json/'+inst_name+'_sim_row.json', 'no-indent')
+net.write_json_to_file('sim_col', 'json/'+inst_name+'_sim_col.json', 'no-indent')
+
+elapsed_time = time.time() - start_time
+print('\n\nelapsed time: '+str(elapsed_time))
