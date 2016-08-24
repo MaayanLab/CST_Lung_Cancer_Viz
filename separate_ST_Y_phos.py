a2167c3ff88b52ee169d8e10dbea2ea913233cd4
cornhundred <doctorcorn2002@gmail.com>
2016-08-24 18:34:40 -0400

    bringing in fixed (removed incorrect cell-line ptms from lung_CL_phos_ratios_non_tuple.tsv), working on this now

--- separate_ST_Y_phos.py
+++ separate_ST_Y_phos.py
@@ -19,14 +19,33 @@ def separate_ST_Y():
   lines = f.readlines()
   f.close()
 
+  fw_st = open('txt/lung_CL_phos_ratios_ST.tsv', 'w')
+  fw_y = open('txt/lung_CL_phos_ratios_Y.tsv', 'w')
+
   for i in range(len(lines)):
 
-    inst_line = lines[i].strip().split('\t')
+    inst_line = lines[i]
 
     if i == 0:
-      print(inst_line)
-      print(len(inst_line))
+      fw_st.write(inst_line)
+      fw_y.write(inst_line)
+
+    else:
+
+      split_line = inst_line.strip().split('\t')
+
+      inst_ptm = split_line[0].split('_')
+
+      # print(len(inst_ptm))
+
+      if len(inst_ptm) < 2:
+        print(inst_ptm)
+
+      # print(split_line[0])
+      # print(inst_ptm)
 
 
+  fw_st.close()
+  fw_y.close()
 
 main()
