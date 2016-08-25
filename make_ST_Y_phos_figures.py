b004c9a87ed38d2e88655146d0bf2ed74c62751c
cornhundred <doctorcorn2002@gmail.com>
2016-08-24 23:04:25 -0400

    made two simple pages, going to bring back clustergrammer python module that is capable of dealing with nan values

--- make_ST_Y_phos_figures.py
+++ make_ST_Y_phos_figures.py
@@ -3,12 +3,13 @@ from clustergrammer import Network
 from copy import deepcopy
 
 all_filenames = ['lung_CL_phos_ratios_ST', 'lung_CL_phos_ratios_Y']
-
+keep_qn_sum = 350
+zscore_thresh = 1.85
 
 for inst_filename in all_filenames:
 
   if inst_filename == 'lung_CL_phos_ratios_ST':
-    num_reach = 4
+    num_reach = 3
   else:
     num_reach = 2
 
@@ -21,17 +22,15 @@ for inst_filename in all_filenames:
   net.normalize(axis='col', norm_type='qn')
 
   # only keep most differentially expressed genes
-  net.filter_N_top('row', 250, 'sum')
+  net.filter_N_top('row', keep_qn_sum, 'sum')
 
   # take zscore
   net.normalize(axis='row', norm_type='zscore', keep_orig=True)
 
   net.swap_nan_for_zero()
 
-  # # original
-  # net.filter_threshold('row', 1.75, 4)
   # # modified version
-  net.filter_threshold('row', 1.75, num_reach)
+  net.filter_threshold('row', zscore_thresh, num_reach)
 
   # views = ['N_row_sum','N_row_var']
   views = []
@@ -41,3 +40,9 @@ for inst_filename in all_filenames:
 
   json_filename = 'json/' + inst_filename + '.json'
   net.write_json_to_file('viz', json_filename)
+
+  json_filename = 'json/' + inst_filename + '_sim_row.json'
+  net.write_json_to_file('sim_row', json_filename)
+
+  json_filename = 'json/' + inst_filename + '_sim_col.json'
+  net.write_json_to_file('sim_col', json_filename)
