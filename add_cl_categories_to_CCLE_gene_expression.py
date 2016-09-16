efdb7c7707f673ed67f7871855f2e7365337d2ea
cornhundred <doctorcorn2002@gmail.com>
2016-09-16 13:12:08 -0400

    taking a break since I need to make plex dictionary and there is not much time before lab meeting

--- add_cl_categories_to_CCLE_gene_expression.py
+++ add_cl_categories_to_CCLE_gene_expression.py
@@ -3,10 +3,22 @@ def main():
   This will add cell line category information (including plexes and
   gene-expression groups to the gene expression data from CCLE)
   '''
+  from clustergrammer import Network
+  net = Network()
 
+  # load original CCLE gene expression data for CST lung cancer cell lines
   filename = 'CCLE_gene_expression/CCLE_NSCLC_all_genes.txt'
   f = open(filename, 'r')
   lines = f.readlines()
   f.close()
+
+  # load cell line info
+  cl_info = net.load_json_to_dict('cell_line_info/cell_line_muts.json')
+
+  # write to new file
+  new_file = 'CCLE_gene_expression/CCLE_NSCLC_cats_all_genes.txt'
+  fw = open(new_file, 'w')
+
+  fw.close()
 
 main()
