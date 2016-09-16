afeef559d4c9e76e1ceaca1c375d4bf970d699eb
cornhundred <doctorcorn2002@gmail.com>
2016-09-16 12:58:18 -0400

    have example phos and expression clustergrams working for homepage, adding categories to cell lines

--- make_cst_homepage_figures.py
+++ make_cst_homepage_figures.py
@@ -2,7 +2,10 @@ def main():
 
   make_phos_homepage_viz()
 
+  make_exp_homepage_viz()
+
 def make_phos_homepage_viz():
+
   from clustergrammer import Network
   net = Network()
 
@@ -19,17 +22,29 @@ def make_phos_homepage_viz():
 
   net.swap_nan_for_zero()
 
-  # threshold filter
+  # threshold filter PTMs
   net.filter_threshold('row', threshold=1.75, num_occur=3)
 
   views = ['N_row_sum','N_row_var']
   net.make_clust(dist_type='cos',views=views, dendro=True,
                  sim_mat=True, calc_cat_pval=False)
 
-  net.write_json_to_file('viz', 'json/mult_view.json', 'indent')
+  net.write_json_to_file('viz', 'json/homepage_phos.json', 'indent')
 
+def make_exp_homepage_viz():
 
-# expression
-# net.filter_threshold('row', threshold=3.0, num_occur=4)
+  from clustergrammer import Network
+  net = Network()
+
+  net.load_file('CCLE_gene_expression/CCLE_NSCLC_all_genes.txt')
+
+  # threshold filter expression
+  net.filter_threshold('row', threshold=3.0, num_occur=4)
+
+  views = ['N_row_sum', 'N_row_var']
+  net.make_clust(dist_type='cos',views=views, dendro=True,
+                 sim_mat=True, calc_cat_pval=False)
+
+  net.write_json_to_file('viz', 'json/homepage_exp.json', 'indent')
 
 main()
