ee9fe8d2058aa4f509fc8eb80a8e6c5f3701c525
Nick Fernandez <nickfloresfernandez@gmail.com>
2016-10-13 11:37:34 -0400

    set up custom pdist calculation using for loop, gives the same result as pdist

--- notebooks/test_pairwise_complete_dist.py
+++ notebooks/test_pairwise_complete_dist.py
@@ -3,22 +3,26 @@ def main():
 
   data_type = 'ptm_none'
   dist_metric = 'euclidean'
-  compare_pdist_to_custom_sim_mat(data_type=data_type, dist_metric=dist_metric)
+  compare_pdist_to_custom_dist_mat(data_type=data_type, dist_metric=dist_metric)
 
-def compare_pdist_to_custom_sim_mat(data_type='ptm_none', dist_metric='euclidean'):
+def compare_pdist_to_custom_dist_mat(data_type='ptm_none', dist_metric='euclidean'):
   '''
-  calculate cell line similarity based on data_type (e.g. expression) with
+  calculate cell line distance based on data_type (e.g. expression) with
   optional filtering and normalization
   '''
 
   filename = '../lung_cellline_3_1_16/lung_cl_all_ptm/precalc_processed/' + \
              data_type + '.txt'
 
-  sim_pdist = calc_pdist_sim(filename, data_type, dist_metric)
+  dist_pdist = calc_pdist_dist(filename, data_type, dist_metric)
+  dist_custom = calc_custom_dist(filename, data_type, dist_metric)
 
-  sim_custom = calc_custom_sim(filename, data_type, dist_metric)
+  difference = dist_pdist - dist_custom
 
-def calc_custom_sim(filename, data_type, dist_metric):
+  print('no difference between custom calculation and pdist calculation')
+  print(sum(difference))
+
+def calc_custom_dist(filename, data_type, dist_metric):
   import numpy as np
   import scipy.spatial.distance as dist_fun
 
@@ -31,21 +35,33 @@ def calc_custom_sim(filename, data_type, dist_metric):
 
   # write for-loop to calculate custom distance matrix and compare result
   # to pdist
-  for col_1 in cols:
+  num_calc = 0
+  for i in range(len(cols)):
 
-    vect_1 = df[col_1]
-    vect_2 = vect_1
+    col_1 = cols[i]
 
-    if dist_metric == 'euclidean':
-      inst_dist = dist_fun.euclidean(vect_1, vect_2)
-    elif dist_metric == 'cosine':
-      inst_dist = dist_fun.cosine(vect_1, vect_2)
+    for j in range(len(cols)):
 
-    print(inst_dist)
+      if j > i:
 
-  # return sim_
+        col_2 = cols[j]
 
-def calc_pdist_sim(filename, data_type, dist_metric):
+        vect_1 = df[col_1]
+        vect_2 = df[col_2]
+
+        if dist_metric == 'euclidean':
+          inst_dist = dist_fun.euclidean(vect_1, vect_2)
+        elif dist_metric == 'cosine':
+          inst_dist = dist_fun.cosine(vect_1, vect_2)
+
+        # save to distance vector
+        dist_vector[num_calc] = inst_dist
+
+        num_calc = num_calc + 1
+
+  return dist_vector
+
+def calc_pdist_dist(filename, data_type, dist_metric):
   from scipy.spatial.distance import pdist, squareform
 
   df = get_df(filename)
@@ -53,9 +69,9 @@ def calc_pdist_sim(filename, data_type, dist_metric):
   # transpose to calc distance matrix of columns
   df = df.transpose()
 
-  sim_pdist = 1 - pdist(df, metric=dist_metric)
+  dist_pdist = pdist(df, metric=dist_metric)
 
-  return sim_pdist
+  return dist_pdist
 
 def get_df(filename):
   from copy import deepcopy
