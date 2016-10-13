c6214391393a538a908186cce7742282423d3bca
Nick Fernandez <nickfloresfernandez@gmail.com>
2016-10-13 12:03:21 -0400

    working on pairwise-complete, will compare to Marks calculations

--- notebooks/test_pairwise_complete_dist.py
+++ notebooks/test_pairwise_complete_dist.py
@@ -5,7 +5,53 @@ def main():
   dist_metric = 'euclidean'
   compare_pdist_to_custom_dist_mat(data_type=data_type, dist_metric=dist_metric)
 
-def compare_pdist_to_custom_dist_mat(data_type='ptm_none', dist_metric='euclidean'):
+  # ###########################################
+  # from scipy.spatial.distance import pdist, squareform
+  # import scipy.spatial.distance as dist_fun
+
+  # dist_metric = 'euclidean'
+
+  # import numpy as np
+  # import pandas as pd
+  # vect_1 = np.zeros(10,)
+  # vect_2 = np.zeros(10,) + 1
+
+  # vect_1[1] = np.NaN
+  # vect_2[9] = np.NaN
+
+  # print(vect_1)
+  # print(vect_2)
+
+  # mat = np.vstack((vect_1, vect_2)).transpose()
+
+  # print(mat)
+
+  # df = pd.DataFrame(mat)
+
+  # df = df.dropna(axis=0)
+
+  # print('\nshape after dropping NaNs')
+  # print(df.shape)
+
+  # df_mean = df.mean(axis=1)
+
+  # print(df_mean)
+
+  # df = df.transpose()
+  # dist_pdist = pdist(df, metric=dist_metric)
+
+  # print('\npdist Distance of two vectors')
+  # print(dist_pdist)
+
+  # df = df.transpose()
+
+  # inst_dist = dist_fun.euclidean(df[0], df[1])
+  # print('\ncustom distance ')
+  # print(inst_dist)
+
+
+def compare_pdist_to_custom_dist_mat(data_type='ptm_none',
+   dist_metric='euclidean', swap_nan=True):
   '''
   calculate cell line distance based on data_type (e.g. expression) with
   optional filtering and normalization
@@ -15,18 +61,24 @@ def compare_pdist_to_custom_dist_mat(data_type='ptm_none', dist_metric='euclidea
              data_type + '.txt'
 
   dist_pdist = calc_pdist_dist(filename, data_type, dist_metric)
-  dist_custom = calc_custom_dist(filename, data_type, dist_metric)
+  dist_custom = calc_custom_dist(filename, data_type, dist_metric,
+                                 swap_nan=swap_nan)
 
   difference = dist_pdist - dist_custom
 
-  print('no difference between custom calculation and pdist calculation')
+  print('\nno difference between custom calculation and pdist calculation')
+  print('--------------------------------------------------------------------')
+  print(dist_pdist[:10])
+  print(dist_custom[:10])
   print(sum(difference))
 
-def calc_custom_dist(filename, data_type, dist_metric):
+def calc_custom_dist(filename, data_type, dist_metric, swap_nan=True):
   import numpy as np
+  import pandas as pd
   import scipy.spatial.distance as dist_fun
+  from scipy.spatial.distance import pdist
 
-  df = get_df(filename)
+  df = get_df(filename, swap_nan)
 
   rows = df.index.tolist()
   cols = df.columns.tolist()
@@ -49,13 +101,24 @@ def calc_custom_dist(filename, data_type, dist_metric):
         vect_1 = df[col_1]
         vect_2 = df[col_2]
 
-        if dist_metric == 'euclidean':
-          inst_dist = dist_fun.euclidean(vect_1, vect_2)
-        elif dist_metric == 'cosine':
-          inst_dist = dist_fun.cosine(vect_1, vect_2)
+        mat = np.vstack((vect_1, vect_2)).transpose()
+        df_small = pd.DataFrame(mat)
+
+        # always dropna (nans will be optionally swapped out elsewhere)
+        df_small = df_small.dropna(axis=0)
+
+        # calc distance using pdist (only two vectors)
+        df_small = df_small.transpose()
+        dist_pdist = pdist(df_small, metric=dist_metric)
+
+        # # calculating distances of two vectors (using pdist instead)
+        # if dist_metric == 'euclidean':
+        #   inst_dist = dist_fun.euclidean(vect_1, vect_2)
+        # elif dist_metric == 'cosine':
+        #   inst_dist = dist_fun.cosine(vect_1, vect_2)
 
         # save to distance vector
-        dist_vector[num_calc] = inst_dist
+        dist_vector[num_calc] = dist_pdist
 
         num_calc = num_calc + 1
 
@@ -64,7 +127,7 @@ def calc_custom_dist(filename, data_type, dist_metric):
 def calc_pdist_dist(filename, data_type, dist_metric):
   from scipy.spatial.distance import pdist, squareform
 
-  df = get_df(filename)
+  df = get_df(filename, swap_nan=True)
 
   # transpose to calc distance matrix of columns
   df = df.transpose()
@@ -73,14 +136,15 @@ def calc_pdist_dist(filename, data_type, dist_metric):
 
   return dist_pdist
 
-def get_df(filename):
+def get_df(filename, swap_nan=True):
   from copy import deepcopy
   from clustergrammer import Network
   net = deepcopy(Network())
 
   # load file and export dataframe
   net.load_file(filename)
-  net.swap_nan_for_zero()
+  if swap_nan == True:
+    net.swap_nan_for_zero()
   tmp_df = net.dat_to_df()
 
   df = tmp_df['mat']
