0398b46d4b535e5c3dcd866169040606a93ae9f0
Nick Fernandez <nickfloresfernandez@gmail.com>
2016-10-13 10:17:14 -0400

    added cl-plex distance comparison

--- notebooks/compare_cl_distnaces.py
+++ notebooks/compare_cl_distnaces.py
@@ -6,21 +6,24 @@ def main():
   affect the distances (and/or similarities) between all cell line pairs in
   gene-expression space
   '''
+
+  compare_cl_data_to_ptm(exp_type='exp_none')
+
+  compare_cl_data_to_ptm(exp_type='exp-plex')
+
+def compare_cl_data_to_ptm(exp_type='exp_none', mantel_method='pearson'):
+
   # compare similarity matrices of cell lines based on different processed
   # versions of exp and ptm data
-
+  #########################################################
   multiple_dist_metrics = ['euclidean','cosine']
-
-  # I will try out various version of expression data
-  # 1) all expression data
-  # 2) normalized and filtered
-  # 3) plex-data cell line (dimensions are plexes)
-  exp_type = 'exp_none'
-  mantel_method = 'pearson'
 
   for inst_metric in multiple_dist_metrics:
     compare_exp_to_various_ptm_versions(exp_type=exp_type,
        dist_metric=inst_metric, mantel_method=mantel_method)
+
+def compare_plex_to_exp():
+  mantel_test('exp-plex', 'exp_none')
 
 def compare_exp_to_various_ptm_versions(exp_type='exp_none',
       dist_metric='euclidean', mantel_method='pearson'):
@@ -75,8 +78,15 @@ def mantel_test(data_1, data_2, perms=10000, tail='upper',
   print('compare ' + data_1 + ' to ' + data_2)
 
   # calculate similarity matrices of both matrices
-  sim_1 = calc_cl_sim(data_type=data_1, dist_metric=dist_metric)
-  sim_2 = calc_cl_sim(data_type=data_2, dist_metric=dist_metric)
+  dist_metric_1 = dist_metric
+  dist_metric_2 = dist_metric
+  if data_1 == 'exp-plex':
+    dist_metric_1 = 'jaccard'
+  if data_2 == 'exp-plex':
+    dist_metric_2 = 'jaccard'
+
+  sim_1 = calc_cl_sim(data_type=data_1, dist_metric=dist_metric_1)
+  sim_2 = calc_cl_sim(data_type=data_2, dist_metric=dist_metric_2)
 
   # pearson or spearman
   results = Mantel.test(sim_1, sim_2, perms=perms, tail='upper', method=mantel_method)
@@ -91,7 +101,6 @@ def calc_cl_sim(data_type='exp_none', dist_metric='euclidean'):
   calculate cell line similarity based on data_type (e.g. expression) with
   optional filtering and normalization
   '''
-
   from scipy.spatial.distance import pdist, squareform
   from clustergrammer import Network
   from copy import deepcopy
@@ -107,7 +116,7 @@ def calc_cl_sim(data_type='exp_none', dist_metric='euclidean'):
 
   df = tmp_df['mat']
 
-  print('shape'+ str(df.shape))
+  print('data_type: ' + data_type + ' dist_metric: ' + dist_metric + ' shape'+ str(df.shape))
 
   # transpose to calc distance matrix of columns
   df = df.transpose()
