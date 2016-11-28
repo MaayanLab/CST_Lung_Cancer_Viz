c4b329944878467ccbd2f71bbd0008ad64a84dd9
cornhundred <nickfloresfernandez@gmail.com>
2016-11-28 13:36:17 -0500

    working on loading data in same way as bio-replicate-corr calc

--- notebooks/check_ppi_top_correlation.py
+++ notebooks/check_ppi_top_correlation.py
@@ -1,7 +1,34 @@
 def main():
+
   load_ppi()
 
+  inst_type = 'ptm45'
+
+  calc_top_corr(inst_type)
+
+def calc_top_corr(data_type):
+  ''' calc top correlations '''
+  from scipy.spatial.distance import pdist, squareform
+  from clustergrammer import Network
+  from copy import deepcopy
+
+
+  filename = '../lung_cellline_3_1_16/lung_cl_all_ptm/precalc_processed/' + \
+             data_type + '.txt'
+
+  # print('\n')
+  # print(data_type)
+  # print('-----------------')
+  # load file and export dataframe
+  net = deepcopy(Network())
+  net.load_file(filename)
+  net.swap_nan_for_zero()
+  tmp_df = net.dat_to_df()
+  df = tmp_df['mat']
+
+
 def load_ppi():
+  ''' load PPI network '''
   filename = '../PPI/PPI.sig'
 
   f = open(filename, 'r')
