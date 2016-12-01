9ca84130c309e23d3d2f8abc5b91950a41515372
cornhundred <nickfloresfernandez@gmail.com>
2016-12-01 14:42:27 -0500

    calculated common dist, workin on reset of qn and testing results

--- notebooks/improved_quantile_normalization.py
+++ notebooks/improved_quantile_normalization.py
@@ -30,6 +30,12 @@ def load_data_as_df(data_type):
   return df
 
 def iqn_df(df, axis='row', keep_orig=False):
+
+  com_dist = calc_common_dist(df)
+
+  print(com_dist.shape)
+
+def calc_common_dist(df):
   '''
   Calculate quantile normalization and remove the influence of missing data
   '''
@@ -57,11 +63,16 @@ def iqn_df(df, axis='row', keep_orig=False):
     # get non-nan values and sort in place
     inst_series = df[inst_col]
     inst_series = inst_series.dropna()
+    inst_series.sort_values(inplace=True)
+
+    # print('there are ' + str(len(inst_series)) + ' values ')
+    # print(inst_series)
+    # print('\n\n')
 
     # save to dictionary
     meas_col[inst_col] = inst_series
 
-    print(inst_col + ' column has ' + str(len(inst_series)) + ' measured values')
+    # print(inst_col + ' column has ' + str(len(inst_series)) + ' measured values')
 
   # map measurements from each col onto com_dist_len number of measuremesnts so
   # that they can be averaged into a common distribution series with duplicate
@@ -86,6 +97,8 @@ def iqn_df(df, axis='row', keep_orig=False):
     list_values = []
     list_indexes = []
 
+    # assign mapped indexes to sorted values: map_series_tmp
+    ############################################################
     # gather sorted indexes and values
     print('gather sorted indexes and values')
     # i is the original sorted index of the data
@@ -95,7 +108,7 @@ def iqn_df(df, axis='row', keep_orig=False):
       inst_value = inst_series[i]
       list_values.append(inst_value)
 
-      # map i is the mapped sorted index - mapped so taht the current index
+      # map i is the mapped sorted index - mapped so that the current index
       # fits into the common index series
       map_i = int( round( (i/float(inst_series_len))*com_dist_len ) )
 
@@ -106,6 +119,8 @@ def iqn_df(df, axis='row', keep_orig=False):
     # save values and indexes to series
     map_series_tmp[inst_col] = pd.Series(list_values, index=list_indexes)
 
+    # make real mapped series: map_series
+    ############################################################
     # average repeat values
     for i in range(com_dist_len):
 
@@ -122,15 +137,18 @@ def iqn_df(df, axis='row', keep_orig=False):
       # add this value to the series
       map_series[inst_col][map_i] = inst_value
 
-  # add map series to dataframe com_dist
+  # add map_series to dataframe com_dist
   for inst_col in all_col:
+    # transfer from dictionary to dataframe
     com_dist[inst_col] = map_series[inst_col]
 
   # average the columns, axis = 1
   com_dist      = com_dist.mean(axis=1)
   com_dist_dict = com_dist.to_dict()
 
-  print(com_dist)
-  print(type(com_dist))
+  com_dist.to_csv('intermediate_data/ptm45_common_distribution.txt', sep='\t')
+
+  # return the common distribution
+  return com_dist
 
 main()
